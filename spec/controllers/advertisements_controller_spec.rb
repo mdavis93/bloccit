0d9de952d85d0dc1203099e93c5011d2814ea9f9
Michael Davis <mdavis93@hotmail.com>
2017-11-06 15:16:18 -0600

    Assignment 18: COMPLETE

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -25,33 +25,53 @@ RSpec.describe AdvertisementsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show, {id: my_ad.id}
+      get :show, params: {id: my_ad.id}
       expect(response).to have_http_status(:success)
     end
 
     it "renders the #show view" do
-      get :show, {id: my_ad.id}
-      expect(response). have_http_status(:success)
+      get :show, params: {id: my_ad.id}
+      expect(response).to have_http_status(:success)
     end
 
     it "assigns my_ad to @advertisement" do
-      get :show, {id: my_ad.id}
+      get :show, params: {id: my_ad.id}
       expect(assigns(:advertisement)).to eq(my_ad)
     end
   end
 
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @advertisement" do
+      get :new
+      expect(assigns(:advertisement)).not_to be_nil
+    end
+  end
+
+  describe "POST #create" do
+    it "assigns the new advertisement to @advertisement" do
+      post :create, params: {advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: rand(30..100)}}
+      expect(assigns(:advertisement)).to eq Advertisement.last
+    end
+
+    it 'increases the number of advertisements by 1' do
+      post :create, params: {advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: rand(30..100)}}
+      change(Advertisement,:count).by(1)
+    end
+
+    it "redirects to the new advertisement" do
+      post :create, params: {advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: rand(30..100)}}
+      expect(response).to redirect_to Advertisement.last
+    end
+  end
 
 end
