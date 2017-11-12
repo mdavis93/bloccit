6e0698941f92c46066baddacd9ce76bd3cf1d79c
Michael Davis <mdavis93@hotmail.com>
2017-11-12 11:44:00 -0600

    Checkpoint 23: COMPLETE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110010040) do
+ActiveRecord::Schema.define(version: 20171112170413) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20171110010040) do
     t.string "name"
     t.string "description"
     t.boolean "public", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
