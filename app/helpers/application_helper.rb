7fdb72543bd805cbf74f1fbbc155d0f5d84ae358
Michael Davis <mdavis93@hotmail.com>
2017-11-12 08:49:52 -0600

    Checkpoint 22: COMPLETE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def form_group_tag(errors, &bloc)
+    css_class = 'form_group'
+    css_class << ' has-error' if errors.any?
+    content_tag :div, capture(&bloc), class: css_class
+  end
 end
