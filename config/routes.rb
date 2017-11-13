44d59ecc0c3c932f814bbfb0f984efea4dcc7d00
Michael Davis <mdavis93@hotmail.com>
2017-11-12 22:57:47 -0600

    Checkpoint 26: COMPLETE

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
-    resources :sponsored_posts, except: [:index]
   end
 
   resources :users, only: [:new, :create]
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,34 @@
 
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
+# Create Topics
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
     description: RandomData.random_paragraph
   )
 end
-
 topics = Topic.all
 
+# Create Posts
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
 end
-
 posts = Post.all
 
 100.times do
@@ -34,19 +45,14 @@ posts = Post.all
   )
 end
 
-50.times do
-  SponsoredPost.create!(
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph,
-    price: rand(50..200)
-  )
-end
-
-sponsored_posts = SponsoredPost.all
+user = User.first
+user.update_attributes!(
+  email: 'mdavis93@gmail.com',
+  password: 'helloworld'
+)
 
 puts "Seed Finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created."
-puts "#{SponsoredPost.count} sponsored posts created."
 puts "#{Comment.count} comments created."
