3fbbf3854d97466aec0c574f356299c49328aa98
Michael Davis <mdavis93@hotmail.com>
2017-11-15 15:34:32 -0600

    Assignment 31: TDD Missing

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def user_is_authorized_for_post?(post)
     current_user && (current_user == post.user || current_user.admin?)
   end
+
+  def user_has_posts_or_comments?(user)
+    user.posts.count > 0 || user.comments.count > 0
+  end
 end
